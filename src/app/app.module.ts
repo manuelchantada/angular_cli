bb38d89a920cf6281a3fe8b6ee7366ae5e48e1bc
manuelchantada <manuelchantada@hotmail.com>
2019-04-21 19:48:19 -0300

    angular CLI tutorial finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
+    FormsModule,  
     AppRoutingModule
   ],
   providers: [],
